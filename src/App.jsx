1ab931d83d784ae365f74d999c1cb4035c74ff03
shraddha singh <shraddhasingh@globussoft.in>
2024-06-09 13:48:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import ChangeAmount from "./components/ChangeAmount";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <ChangeAmount />
+    </>
+  );
+};
+
+export default App;
